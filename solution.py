110d7edd4ed6c825406aaccdd8b876694b275027
D-coder <110387042+dcoder201@users.noreply.github.com>
2022-10-27 13:05:12 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def inSequence(self, A, B, C):
+        # code here
+        if C == 0:
+            return 1 if A == B else 0
+        n = (B-A)/C + 1
+        return 1 if n > 0 and int(n) == n else 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        A, B, C = [int(x) for x in input().split()]
+        
+        ob = Solution();
+        print(ob.inSequence(A, B, C))
+# } Driver Code Ends
